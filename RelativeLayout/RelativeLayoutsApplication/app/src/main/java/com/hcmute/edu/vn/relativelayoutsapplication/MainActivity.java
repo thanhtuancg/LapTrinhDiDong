e4c7031df3992cbc22d9fa0fdb72b624f954b8af
Tuấn Huỳnh <huynhthanhtuan2206@gmail.com>
2023-02-16 16:03:50 +0700

    thêm relativelayout

---
+++ RelativeLayout/RelativeLayoutsApplication/app/src/main/java/com/hcmute/edu/vn/relativelayoutsapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hcmute.edu.vn.relativelayoutsapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relativelayout);
+    }
+}
