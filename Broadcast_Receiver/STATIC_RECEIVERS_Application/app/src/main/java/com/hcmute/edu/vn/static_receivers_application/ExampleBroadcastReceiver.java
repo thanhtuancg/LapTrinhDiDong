1ace59b7f618d5886edd0ba26b6db3bc7b188a3a
HT-Tuan <huynhthanhtuan2206@gmail.com>
2023-03-17 15:56:27 +0700

    add project using Static Receiver

---
+++ Broadcast_Receiver/STATIC_RECEIVERS_Application/app/src/main/java/com/hcmute/edu/vn/static_receivers_application/ExampleBroadcastReceiver.java
@@ -0,0 +1,20 @@
+package com.hcmute.edu.vn.static_receivers_application;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+import android.net.ConnectivityManager;
+
+public class ExampleBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Toast.makeText(context, "Boot completed", Toast.LENGTH_SHORT).show();
+        }
+
+        if (ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())) {
+            Toast.makeText(context, "Connectivity changed", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
